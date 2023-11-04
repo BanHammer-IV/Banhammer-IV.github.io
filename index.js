f66f2e49a76a32591af0e57d6b8454adb95e86dd
BanHammer-IV <BanHammer.Developer@hotmail.com>
2023-11-03 22:28:30 -0700

    Agregada la funcion de los boton atras y adelante

---
+++ index.js
@@ -0,0 +1,10 @@
+let btnLeft = document.getElementById("btnLeft");
+let btnRigth = document.getElementById("btnRigth");
+
+btnLeft.addEventListener("click", ()=>{
+    console.log("Izquierda");
+});
+
+btnRigth.addEventListener("click", ()=>{
+    console.log("Derecha");
+});
