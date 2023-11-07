837de41747d60e976407d07b4661d409607a2eeb
BanHammer-IV <BanHammer.Developer@hotmail.com>
2023-11-06 20:50:40 -0700

    Query's de MySQL que se usaran en el proyecto

--- Query_BD/proyectocine.sql
+++ Query_BD/proyectocine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-11-2023 a las 09:07:44
+-- Tiempo de generación: 06-11-2023 a las 00:08:41
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -45,25 +45,66 @@ INSERT INTO `empleados` (`id`, `nombre`, `usuario`, `pass`, `puesto`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `locaciones`
+--
+
+CREATE TABLE `locaciones` (
+  `direccion` varchar(100) NOT NULL,
+  `apertura` time NOT NULL,
+  `cierre` time NOT NULL,
+  `cantidad_salas` varchar(100) NOT NULL,
+  `peliculas` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peliculas`
+--
+
+CREATE TABLE `peliculas` (
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(2000) NOT NULL,
+  `director` varchar(30) NOT NULL,
+  `clasificacion` varchar(30) NOT NULL,
+  `genero` varchar(50) NOT NULL,
+  `salas` varchar(30) NOT NULL,
+  `horario` time NOT NULL,
+  `estreno` date NOT NULL,
+  `idiomas` varchar(50) NOT NULL,
+  `trailer` varchar(2000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `promociones`
+--
+
+CREATE TABLE `promociones` (
+  `nombre` varchar(50) NOT NULL,
+  `inicio` datetime NOT NULL,
+  `finalizacion` datetime NOT NULL,
+  `precio` float NOT NULL,
+  `contenido` varchar(2000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nombre` varchar(30) NOT NULL,
+  `telefono` varchar(10) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `nacimiento` date NOT NULL,
   `usuario` varchar(20) NOT NULL,
-  `pass` varchar(20) NOT NULL
+  `pass` varchar(20) NOT NULL,
+  `metodo_pago` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `pass`) VALUES
-(1, 'carlos', 'CarlosYZR', '123'),
-(2, 'Laura', 'Laura', '123'),
-(4, 'juan', 'Juan1234', '123'),
-(5, 'Julian', 'Julien', '123');
 
 --
 -- Índices para tablas volcadas
@@ -95,7 +136,7 @@ ALTER TABLE `empleados`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
